6cbc3c5fd33cfe519e8f9322f42397545889bfe7
Gustavo Muratalla <gusmuratalla@gmail.com>
2021-07-04 21:17:49 -0700

    fixed issue if notesArray was zero then it would thro error

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -20,7 +20,7 @@ router.get('/notes/:id', (req, res) => {
 //post route to upload to our json
 router.post('/notes', (req, res) => {
     // set id based on what the next index of the array will be
-    req.body.id = notesArray.length.toString();
+    req.body.id = notesArray.length.toString() || 0;
 
     if (!validateNote(req.body)) {
         res.status(400).send('The note is not properly formatted.');
